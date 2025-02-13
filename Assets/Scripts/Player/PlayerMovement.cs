7816bd8b4fa563330279a84a66a6a5c696929576
Jacob Seiberg <jakeseiberg@gmail.com>
2025-02-13 16:59:52 +0100

    Added basic movement functionality

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        handleMovement();
+    }
+
+    void handleMovement(){
+        Vector3 pos = transform.position;
+        if(Input.GetKey(KeyCode.D)){
+            pos.x += speed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.A)){
+            pos.x -= speed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.W)){
+            pos.y += speed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.S)){
+            pos.y -= speed * Time.deltaTime;
+        }  
+        
+        transform.position = pos;
+    }
+}
