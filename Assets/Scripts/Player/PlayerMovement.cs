488a05b441426a428f43a13dd1bfc5eccfa88914
Jacob Seiberg <jakeseiberg@gmail.com>
2025-02-13 17:10:42 +0100

    basic animations

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
+    public Sprite[] characterSprites;
+    private SpriteRenderer playerSprite;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerSprite = GetComponent<SpriteRenderer>();   
     }
 
     // Update is called once per frame
@@ -19,15 +21,19 @@ public class PlayerMovement : MonoBehaviour
         Vector3 pos = transform.position;
         if(Input.GetKey(KeyCode.D)){
             pos.x += speed * Time.deltaTime;
+            playerSprite.sprite = characterSprites[3];
         }
         if(Input.GetKey(KeyCode.A)){
             pos.x -= speed * Time.deltaTime;
+            playerSprite.sprite = characterSprites[1];
         }
         if(Input.GetKey(KeyCode.W)){
             pos.y += speed * Time.deltaTime;
+            playerSprite.sprite = characterSprites[2];
         }
         if(Input.GetKey(KeyCode.S)){
             pos.y -= speed * Time.deltaTime;
+            playerSprite.sprite = characterSprites[0];
         }  
         
         transform.position = pos;
