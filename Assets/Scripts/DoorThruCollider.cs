3a0b758d1a355ed800b5b543534cb4397993a6fe
Tyler Garvey <tmgarv26@colby.edu>
2025-02-24 23:49:51 +0100

    door fix which will make it easier to implement in every other level. this shit took years off my life

--- Assets/Scripts/DoorThruCollider.cs
+++ Assets/Scripts/DoorThruCollider.cs
@@ -6,11 +6,11 @@ public class DoorThruCollider : MonoBehaviour
 
     public SpriteRenderer doorSpriteRenderer; // Assign in Inspector
     public Sprite[] doorSprites; // Assign multiple sprites in Inspector
-    public float frameDelay = 0.12f; // Time between frames
+    private float frameDelay = 0.12f; // Time between frames
     public int roomIndexAfterTeleport;
 
-    public float teleportX;
-    public float teleportY;
+    private Vector2 teleportOffset;
+    public GameObject door;
 
     public GameObject player;
 
@@ -18,6 +18,11 @@ public class DoorThruCollider : MonoBehaviour
     
     private bool hasOpened = false;
 
+    void Start()
+    {
+        teleportOffset.x = -4.20183f;
+        teleportOffset.y = 6.33f;
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -61,9 +66,9 @@ public class DoorThruCollider : MonoBehaviour
         yield return new WaitForSeconds(.5f); // Wait before changing
 
         
-        Vector3 newPosition = player.transform.position;
-        newPosition.x = teleportX;  // Set the X position
-        newPosition.y = teleportY;  // Set the Y position
+        Vector3 newPosition = door.transform.position;
+        newPosition.x += teleportOffset.x;  // Set the X position
+        newPosition.y += teleportOffset.y;  // Set the Y position
         player.transform.localPosition = newPosition;  // Apply the new position
         PlayerRoomTracker.SetPlayerRoomIndex(roomIndexAfterTeleport);
 
