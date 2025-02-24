75995e8d0cbde61481ae49d719fc69ca08355482
Jacob Seiberg <jakeseiberg@gmail.com>
2025-02-24 15:44:34 +0100

    fixed update changes

--- Assets/Scripts/Player/Interaction.cs
+++ Assets/Scripts/Player/Interaction.cs
@@ -16,7 +16,7 @@ public class Interaction : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if(Input.GetKey(KeyCode.Space) && LightTime.maxTime > 0){
             LightUp();
