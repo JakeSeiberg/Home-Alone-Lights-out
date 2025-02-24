7f51d7a53b6467fecfe343cb465ac1eca43685d7
Reid Allenstein <reidallenstein@gmail.com>
2025-02-24 16:25:06 +0100

    fixed camera shaking when monsters are gone

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -10,6 +10,7 @@ public class CameraShake : MonoBehaviour
     public CinemachineCamera vCam;
     public GameObject player;
     public GameObject[] enemies;
+    public GameObject outDistance;
     private CinemachineBasicMultiChannelPerlin noise;
 
     public float minNoise = 1;
@@ -30,13 +31,20 @@ public class CameraShake : MonoBehaviour
     void ClosestMonster(){
 
         for (int i = 0; i < enemies.Length; i++){
-            Vector2 a = new Vector2(player.transform.position.x, player.transform.position.y);
-            Vector2 b = new Vector2(enemies[i].transform.position.x, enemies[i].transform.position.y);
-            distance[i] = Vector2.Distance(a, b);
+            if (!enemies[i].activeSelf)
+            {
+                enemies[i] = outDistance;
+            }
+            else{
+                Vector2 a = new Vector2(player.transform.position.x, player.transform.position.y);
+                Vector2 b = new Vector2(enemies[i].transform.position.x, enemies[i].transform.position.y);
+                distance[i] = Vector2.Distance(a, b);
+            }
         }
     }
 
-    void CShake(float closestMonster){
+    void CShake(float closestMonster){ 
+        //TODO: check monster tag for room location, if room lever is switched, remove from array
         if (closestMonster <= 3 && closestMonster > 2){
             noise.FrequencyGain = 1;
         }
